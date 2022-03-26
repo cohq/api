922585e86ac946d88f10d3c700e2e210f953fcb4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-26 13:52:56 -0500

    :sparkles: Update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
-import { Application } from "./depts.ts";
+import { Application, Router } from "./depts.ts";
+
+import { createCount, findCount, updateCount } from "./db.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World";
+const api = new Router();
+
+api.get("/api/simple/:id", async (ctx) => {
+  const key = decodeURIComponent(ctx.params.id);
+
+  const count = await findCount(key);
+
+  if (!count) {
+    ctx.response.body = await createCount(key);
+    return;
+  }
+
+  console.log(`Updating count for ${key}`);
+
+  const updatedCount = await updateCount({
+    _id: count._id,
+    key,
+    value: count.value + 1,
+  });
+
+  ctx.response.body = updatedCount;
 });
+
+app.use(api.routes());
+app.use(api.allowedMethods());
 
 app.addEventListener("listen", ({ hostname, port, secure }) => {
   const protocol = secure ? "https" : "http";
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Count {
+  _id: string;
+  key: string;
+  value: number;
+}
