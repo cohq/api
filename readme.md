895706bed8d0937ca648e4e4019ec4b090a66923
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-26 12:17:35 -0500

    :tada: Begin

---
+++ readme.md
@@ -0,0 +1,17 @@
+# The Count API
+
+Allows you to create simple numeric counters. IaaS, Integer as a Service.
+
+## Docs
+
+TODO
+
+## Drivers
+
+- JavaScript (Node.js/Deno/Browser): [`cohq.js`](https://github.com/cohq/cohq.js)
+
+- Go: [`cohq/cogo`](https://github.com/cohq/cogo)
+
+## Licence
+
+Licensed under the MIT License.
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Application } from "./depts.ts";
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  ctx.response.body = "Hello World";
+});
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  const protocol = secure ? "https" : "http";
+
+  console.log(`Listening on: ${protocol}://${hostname ?? "localhost"}:${port}`);
+});
+
+app.listen({ port: 8000 });
